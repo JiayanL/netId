528b2b0b659564b4a9d90468d9fadb27ee5948c0
Jiayan Luo <jiayanluo2024@u.northwestern.edu>
2023-09-22 14:01:52 -0500

    first commit

---
+++ src/components/StickyHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StickyHeader = () => {
+  return (
+    <div className="sticky top-0 z-10 bg-purple-600 text-white text-center py-4 text-x1 font-bold shadow-md">
+    View My Student ID
+      </div>
+  )
+}
+
+export default StickyHeader;

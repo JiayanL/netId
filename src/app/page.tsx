d8c68de5b43b376e65dffd13f12dc0380608d735
Jiayan Luo <jiayanluo2024@u.northwestern.edu>
2023-09-22 14:04:25 -0500

    className

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="App">
       <StickyHeader />
-      <nav class="sticky top-0 z-10 bg-white p-4 right-0 space-x-4">
+      <nav className ="sticky top-0 z-10 bg-white p-4 right-0 space-x-4">
         <a href="#" class="text-blue-500">New Window</a>
         <a href="#" class="text-blue-500">Help</a>
         <a href="#" class="text-blue-500">Personalize Page</a>
